766c60427316ad4517b30edaebe1cf402baae939
ariel <santa.cruz.unla@gmail.com>
2023-01-24 20:33:26 -0300

    spinner añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,21 @@ export class AppComponent {
 
   listNoticias: any [] = [];
 
+  loading = false;
+
   constructor (private _noticiaService: NoticiaService){
 
   }
 
   buscarNoticias(parametros:any){
+    this.loading = true;
+    this.listNoticias = [];
+
     console.log('soy el padre')
     console.log(parametros)
 
     this._noticiaService.getNoticias(parametros).subscribe(data => {console.log(data);
-    
+    this.loading = false;
     this.listNoticias = data.articles;
 
     })
